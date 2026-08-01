d58fb846f43a5bf00527704be4338a5f4f345e88
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-08-01 11:44:43 +0100

    feat: initialization du project

---
+++ backend/src/main/java/dev/wassim/lexi/LexiApplication.java
@@ -0,0 +1,13 @@
+package dev.wassim.lexi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LexiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/wassim/lexi/LexiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wassim.lexi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LexiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
