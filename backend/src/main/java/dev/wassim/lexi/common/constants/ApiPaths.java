e8306002a9ce60f90074a5f1d2a848bde3c2c5bc
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-08-03 18:30:58 +0100

    feat: creat WordController

--- backend/src/main/java/dev/wassim/lexi/common/constants/ApiPaths.java
+++ backend/src/main/java/dev/wassim/lexi/common/constants/ApiPaths.java
@@ -4,5 +4,5 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 public class ApiPaths {
-    private static final String Api_Words = "api/words";
+    public static final String Api_Words = "api/words";
 }
