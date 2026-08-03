ddd8322c232635761ba31d7f0adc96cc7d7ce96b
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-08-03 18:48:22 +0100

    feat: add findByDay a query methodes

--- backend/src/main/java/dev/wassim/lexi/features/words/repositories/WordRepository.java
+++ backend/src/main/java/dev/wassim/lexi/features/words/repositories/WordRepository.java
@@ -1,9 +1,13 @@
 package dev.wassim.lexi.features.words.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.wassim.lexi.domain.modal.Word;
+import java.time.LocalDate;
+
 
 public interface WordRepository extends JpaRepository<Word , Long> {
-    
+    List<Word> findByDay(LocalDate day);
 }
