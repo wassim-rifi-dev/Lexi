5c7babbed3118653fe6866a163845f8c4d025331
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-08-04 19:21:41 +0100

    feat: create GeminiClient

--- backend/src/main/java/dev/wassim/lexi/gemini/client/GeminiClient.java
+++ backend/src/main/java/dev/wassim/lexi/gemini/client/GeminiClient.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
+import dev.wassim.lexi.gemini.dto.request.GeminiRequest;
+import dev.wassim.lexi.gemini.dto.response.GeminiResponse;
 import lombok.AllArgsConstructor;
 
 @Component
@@ -13,4 +15,16 @@ public class GeminiClient {
 
     @Value("${gemini.api.key}")
     private String key;
+
+    public GeminiResponse generateWords(GeminiRequest request) {
+        return geminiRestClient.post()
+                            .uri(uriBuilder -> uriBuilder
+                                .path("/gemini-2.5-flash:generateContent")
+                                .queryParam("key", key)
+                                .build()
+                            )
+                            .body(request)
+                            .retrieve()
+                            .body(GeminiResponse.class);
+    }
 }
