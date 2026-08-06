1d77cc6c116ca2d426697603f668e282fd987b27
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-08-06 23:35:33 +0100

    feat: create toGeminiResponse and toGeminiResponseJson in GeminiMapper

--- backend/src/main/java/dev/wassim/lexi/gemini/client/GeminiClient.java
+++ backend/src/main/java/dev/wassim/lexi/gemini/client/GeminiClient.java
@@ -6,6 +6,7 @@ import org.springframework.web.client.RestClient;
 import dev.wassim.lexi.gemini.dto.request.GeminiRequest;
 import dev.wassim.lexi.gemini.dto.response.GeminiApiResponse;
 import dev.wassim.lexi.gemini.dto.response.GeminiResponse;
+import dev.wassim.lexi.gemini.mapper.GeminiMapper;
 import lombok.RequiredArgsConstructor;
 import tools.jackson.databind.ObjectMapper;
 
@@ -13,6 +14,7 @@ import tools.jackson.databind.ObjectMapper;
 @RequiredArgsConstructor
 public class GeminiClient {
     private final ObjectMapper objectMapper;
+    private final GeminiMapper geminiMapper;
 
     private String key = "${GEMINI_API_KEY}";
 
--- backend/src/main/java/dev/wassim/lexi/gemini/mapper/GeminiMapper.java
+++ backend/src/main/java/dev/wassim/lexi/gemini/mapper/GeminiMapper.java
@@ -1,8 +1,30 @@
 package dev.wassim.lexi.gemini.mapper;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import dev.wassim.lexi.gemini.dto.request.GeminiRequest;
+import dev.wassim.lexi.gemini.dto.response.GeminiApiResponse;
 
 @Component
 public class GeminiMapper {
-    
+    public GeminiApiResponse toGeminiResponse(String key , GeminiRequest request) {
+        return RestClient.create()
+                        .post()
+                        .uri(
+                            "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash:generateContent?key=" + key
+                        )
+                        .body(request)
+                        .retrieve()
+                        .body(GeminiApiResponse.class);
+    }
+
+    public String toGeminiResponseJson(GeminiApiResponse response) {
+        return response.getCandidates()
+                        .get(0)
+                        .getContent()
+                        .getParts()
+                        .get(0)
+                        .getText();
+    }
 }
