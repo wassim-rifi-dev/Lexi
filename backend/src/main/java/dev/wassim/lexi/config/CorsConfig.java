442065e1179b78769f25167d7d313a81a52e7b1e
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-08-15 18:39:20 +0100

    fix: transforme the corsConfig to use CorsFilter

--- backend/src/main/java/dev/wassim/lexi/config/CorsConfig.java
+++ backend/src/main/java/dev/wassim/lexi/config/CorsConfig.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import lombok.AllArgsConstructor;
 
@@ -16,29 +16,18 @@ public class CorsConfig {
     private final CorsProperties properties;
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
+    public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(
-            properties.getAllowedOrigins()
-        );
-        config.setAllowedMethods(
-            List.of(
-                "*"
-            )
-        );
-        config.setAllowedHeaders(
-            List.of(
-                "*"
-            )
-        );
+        config.setAllowedOrigins(properties.getAllowedOrigins());
+        config.setAllowedMethods(List.of("*"));
+        config.setAllowedHeaders(List.of("*"));
         config.setMaxAge(3600L);
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-
         source.registerCorsConfiguration("/**", config);
 
-        return source;
+        return new CorsFilter(source);
     }
 }
