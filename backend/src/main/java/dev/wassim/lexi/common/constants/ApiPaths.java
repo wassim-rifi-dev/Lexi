208cca7051c01f5ec54d92a24c1fcad4b8ac77c9
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-08-28 00:45:11 +0100

    feat: implement UserController, UserMapper, and UserService for user management

--- backend/src/main/java/dev/wassim/lexi/common/constants/ApiPaths.java
+++ backend/src/main/java/dev/wassim/lexi/common/constants/ApiPaths.java
@@ -5,4 +5,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ApiPaths {
     public static final String Api_Words = "api/words";
+    public static final String API_USER = "api/users";
 }
---
+++ backend/src/main/java/dev/wassim/lexi/features/user/controller/UserController.java
@@ -0,0 +1,12 @@
+package dev.wassim.lexi.features.user.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.wassim.lexi.common.constants.ApiPaths;
+
+@RestController
+@RequestMapping(ApiPaths.API_USER)
+public class UserController {
+    
+}
