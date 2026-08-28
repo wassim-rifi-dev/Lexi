64390aeef9b24d4d87bd074bcdacf9250deff1bd
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-08-28 01:29:23 +0100

    feat: add validation annotations for username, email, and password in RegisterRequest DTO

--- backend/src/main/java/dev/wassim/lexi/features/user/dto/request/RegisterRequest.java
+++ backend/src/main/java/dev/wassim/lexi/features/user/dto/request/RegisterRequest.java
@@ -1,5 +1,8 @@
 package dev.wassim.lexi.features.user.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
@@ -8,9 +11,14 @@ public record RegisterRequest(
 
     String last_name,
 
+    @NotBlank(message = "Username is required")
     String username,
 
+    @NotBlank(message = "Email is required")
+    @Email
     String email,
 
+    @NotBlank(message = "Password is required")
+    @Size(min = 8)
     String password
 ) {}
