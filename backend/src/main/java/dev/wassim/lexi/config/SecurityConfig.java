600ffa7fd6deeeb4af012cf0754e47a32304b2e6
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-09-02 07:03:59 +0100

    feat: add security configuration and launch settings for Spring Boot application

--- backend/src/main/java/dev/wassim/lexi/config/SecurityConfig.java
+++ backend/src/main/java/dev/wassim/lexi/config/SecurityConfig.java
@@ -2,11 +2,18 @@ package dev.wassim.lexi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http.build();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
