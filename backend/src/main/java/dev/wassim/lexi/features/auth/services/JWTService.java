41d9ca60b542d1980ee357e6db7832b445770153
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-09-02 07:51:57 +0100

    feat: implement JWT token creation method with secret key handling

--- backend/src/main/java/dev/wassim/lexi/features/auth/services/JWTService.java
+++ backend/src/main/java/dev/wassim/lexi/features/auth/services/JWTService.java
@@ -1,8 +1,34 @@
 package dev.wassim.lexi.features.auth.services;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import dev.wassim.lexi.domain.modal.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JWTService {
-    
+    @Value("${app.jwt.secret-key}")
+    public String Secret_Key;
+
+    public String createJwtToken(User user) {
+        Key key = Keys.hmacShaKeyFor(
+            Secret_Key.getBytes(StandardCharsets.UTF_8)
+        );
+
+        String token = Jwts.builder()
+                            .subject(user.getEmail())
+                            .claim("username", user.getUsername())
+                            .issuedAt(new Date())
+                            .expiration(new Date(System.currentTimeMillis() + 15 * 60 * 1000))
+                            .signWith(key)
+                            .compact();
+
+        return token;
+    }
 }
